3df99d89cd68400c7c10ceaa5120670d0720f908
Boshika Tara <boshikatara@gmail.com>
2016-03-20 14:41:52 -0700

    Created a server

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+
+var app = express();
+
+var port = process.env.PORT || 4000;
+
+//set-up
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', routes);
+
+app.listen(port, function(err){
+    console.log('running server');
+});
+
+
+module.exports = app;
